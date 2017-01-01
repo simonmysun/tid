1d3fbef894a63616a144d1bb183312994991685a
MY Sun <simonmysun@gmail.com>
2017-01-01 02:40:28 +0100

    Prevent ignoring static folder, rerender even if failed

--- src/frontend/renderers/markdown/renderer.js
+++ src/frontend/renderers/markdown/renderer.js
@@ -48,47 +48,52 @@ updateDom.setUpdateFn((changedNode) => {
 });
 
 const trueRender = (source) => {
-  [...document.getElementsByClassName('update-mark')].forEach((node) => {
-    node.classList.remove('update-mark');
-  });
-  lastUpdatedDom = null;
-  timestamp.push(performance.now());
-  const result = md.render(source);
-  timestamp.push(performance.now());
-  newDom = document.createElement('div');
-  newDom.id = 'main';
-  newDom.className = 'markdown';
-  newDom.innerHTML = result;
-  timestamp.push(performance.now());
-  targetDom = updateDom.update(targetDom, newDom);
-  timestamp.push(performance.now());
-  if (lastUpdatedDom !== null) {
-    while (lastUpdatedDom.nodeType !== 1) {
-      if (lastUpdatedDom.parentNode) {
-        lastUpdatedDom = lastUpdatedDom.parentNode;
-      } else {
-        console.log('???');
-        return;
-      }
-    }
-    void lastUpdatedDom.offsetWidth; // eslint-disable-line
-    lastUpdatedDom.classList.add('update-mark');
-    scrollToElement(lastUpdatedDom);
-  }
-  MathJax.Hub.Queue(['Typeset', MathJax.Hub, targetDom]);
-  MathJax.Hub.Queue(() => {
+  try {
+    [...document.getElementsByClassName('update-mark')].forEach((node) => {
+      node.classList.remove('update-mark');
+    });
+    lastUpdatedDom = null;
     timestamp.push(performance.now());
-    console.log('parse -> new dom -> patch -> mathjax');
-    console.log(timestamp.map((item, key, arr) => arr[key] - arr[key - 1])
-                         .filter(i => i));
-    timestamp.length = 0;
-    rendering = false;
-    if (needRerender) {
-      rendering = true;
-      needRerender = false;
-      trueRender(nextToRender);
+    const result = md.render(source);
+    timestamp.push(performance.now());
+    newDom = document.createElement('div');
+    newDom.id = 'main';
+    newDom.className = 'markdown';
+    newDom.innerHTML = result;
+    timestamp.push(performance.now());
+    targetDom = updateDom.update(targetDom, newDom);
+    timestamp.push(performance.now());
+    if (lastUpdatedDom !== null) {
+      while (lastUpdatedDom.nodeType !== 1) {
+        if (lastUpdatedDom.parentNode) {
+          lastUpdatedDom = lastUpdatedDom.parentNode;
+        } else {
+          console.log('???');
+          return;
+        }
+      }
+      void lastUpdatedDom.offsetWidth; // eslint-disable-line
+      lastUpdatedDom.classList.add('update-mark');
+      scrollToElement(lastUpdatedDom);
     }
-  });
+  } catch (e) {
+    console.log(e);
+  } finally {
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub, targetDom]);
+    MathJax.Hub.Queue(() => {
+      timestamp.push(performance.now());
+      console.log('parse -> new dom -> patch -> mathjax');
+      console.log(timestamp.map((item, key, arr) => arr[key] - arr[key - 1])
+                           .filter(i => i));
+      timestamp.length = 0;
+      rendering = false;
+      if (needRerender) {
+        rendering = true;
+        needRerender = false;
+        trueRender(nextToRender);
+      }
+    });
+  }
 };
 
 const render = (source) => {
