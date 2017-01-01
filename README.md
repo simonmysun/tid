2c10390ddb6cd79f9b4397fc2c6fef5274dc36f1
MY Sun <simonmysun@gmail.com>
2017-01-01 20:01:48 +0100

    some bug fixes and better demo site

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ Take It Down - yet another browser based "editor". There is a [demo page](https:
 ## Why
 I need a more convinient tool to view and edit my lecture notes. But nothing satisfied me. So I write this. 
 
-Features:
+## Features
 * Serve a root and I can browse everything with my browser(PDF, images, videos, audios);
 * Render markdown files, and the Mathematic formulas inside it(with the grammar of $\LaTeX$);
 * When viewing the markdown files, there is a thumbnail, for faster navigating and preview the structure of the file;
 
 ## Install
 ```
-npm install tid -g
+npm i -g tid
 ```
 
 ## Usage
@@ -22,13 +22,11 @@ npm install tid -g
 
   Options:
 
-    -h, --help           output usage information
-    -V, --version        output the version number
-    -B, --no-browser     do not start browser after the service started. defaults to true
-    -C, --chdir [path]   change the working directory, defaults to current directory
-    -c, --config [path]  set config path. defaults to ./deploy.conf
-    -P, --port [n]       specify a port to use, by default the port 3000 will be used
-    -w, --watch          watch file changes and automatically refresh
+    -h, --help          output usage information
+    -V, --version       output the version number
+    -B, --no-browser    do not start browser after the service started. defaults to true
+    -C, --chdir [path]  change the working directory (the default is current directory
+    -P, --port [n]      specify a port to use, by default the port 3000 will be used
 
 ```
 
--- src/frontend/main.js
+++ src/frontend/main.js
@@ -5,6 +5,8 @@ import notify from './notify';
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "styles" }] */
 import styles from './main.css';
 
+const isDemo = false;
+
 const $main = document.getElementById('main');
 const path = decodeURI(location.hash.slice(1));
 const $base = document.createElement('base');
@@ -14,37 +16,13 @@ if (location.hash.length < 2) {
   renderer.bind(path, $main);
   document.title = path;
   $base.setAttribute('href', path);
+  $base.setAttribute('target', '_blank');
   document.getElementsByTagName('head')[0].appendChild($base);
 }
 
 window.addEventListener('hashchange', () => {
   location.hash = path;
 }, false);
-
-const socket = io(location.origin);
-socket.on('connect', () => {
-  socket.emit('subscribe file change', {
-    path,
-  });
-});
-
-socket.on('update', (data) => {
-  switch (data.status) {
-    case '200':
-      renderer.render(data.content, data.path);
-      break;
-    case '400':
-      renderer.render('400 Bad request. ', data.path);
-      notify('400 Bad request. ');
-      break;
-    case '404':
-      renderer.render('404 File not found. ', data.path);
-      notify('404 File not found. ');
-      break;
-    default:
-      break;
-  }
-});
 
 window.addEventListener('storage', (event) => {
   if (event.key === '__broadcast') {
@@ -57,3 +35,34 @@ window.addEventListener('storage', (event) => {
     }
   }
 });
+
+if (isDemo) {
+  if (window.parent !== window) {
+    window.parent.postMessage('ready', window.location.origin);
+  }
+} else {
+  const socket = io(location.origin);
+  socket.on('connect', () => {
+    socket.emit('subscribe file change', {
+      path,
+    });
+  });
+
+  socket.on('update', (data) => {
+    switch (data.status) {
+      case '200':
+        renderer.render(data.content, data.path);
+        break;
+      case '400':
+        renderer.render('400 Bad request. ', data.path);
+        notify('400 Bad request. ');
+        break;
+      case '404':
+        renderer.render('404 File not found. ', data.path);
+        notify('404 File not found. ');
+        break;
+      default:
+        break;
+    }
+  });
+}
